fccdd588af05a1c1e25e09019c455912e1ab8525
Madridistasxx <zhangjq619731@gmail.com>
2026-05-12 12:57:31 +0200

    Add command for test

--- scripts/LC.sh
+++ scripts/LC.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python WrappingNet.py --gpus 0 --epochs 1000 --epochs_sphere 200 --latent_dim 512 --lr 1e-5 --data_name "manifold40" --model_name "LC"
+uv run test.py --gpus 0 --epochs 10 --epochs_sphere 5 --latent_dim 512 --lr 1e-5 --data_name "tools" --model_name "LC"
